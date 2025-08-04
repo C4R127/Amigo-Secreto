63e734e6d1be15bd68aa5aa35afe949ff308dbf0
Carlos Barra <carloseduardobc27@gmail.com>
2025-08-04 03:19:43 -0500

    Actualizacion del metodo agregarAmigo

--- app.js
+++ app.js
@@ -9,27 +9,6 @@ function asignarTextoElemento(Elemento, texto){
     titulo.innerHTML = texto;
 }
 
-
-//corregir codigo en proceso
-function agregarAmigo() {
-    // Obtener el valor del campo de entrada y agregarlo a la lista
-    let amigoSecreto = document.getElementById('amigo').value;
-    // Validar que el campo no esté vacío
-    if (amigoSecreto.trim() !== '') {
-        listaDeAmigos.push(amigoSecreto);
-        document.getElementById('amigo').value = ''; // Limpiar el campo de entrada
-        actualizarLista();
-    } else {
-        alert('Por favor, ingrese un nombre válido.');
-    }
-}
-
-//Corregir codigo en proceso
-function actualizarLista() {
-    let listaDeAmigos = document.querySelector('.lista-amigos');
-    
-}
-
 function condicionesIniciales() {
     asignarTextoElemento('h1', 'Amigo Secreto');
     asignarTextoElemento('h2', 'Digite el nombre de sus Amigos');
@@ -37,10 +16,17 @@ function condicionesIniciales() {
 }
 
 //corregir codigo en proceso
-function sortearAmigos() {
-    let 
-    if (listaDeAmigos.length > 0) {
-        listaDeAmigos.sort();
+function agregarAmigo() {
+    // Obtener el valor del campo de entrada y agregarlo a la lista
+    let amigoSecreto = document.getElementById('amigo').value;
+    // Validar que el campo no esté vacío
+    if (amigoSecreto === '') {
+        alert('Por favor, ingrese un nombre.');
+        return;
+    } else {
+        listaDeAmigos.push(amigoSecreto);
+        document.getElementById('amigo').value = ''; // Limpiar el campo de entrada
         actualizarLista();
     }
 }
+
