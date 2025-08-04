a1d55071f5203004dca58ac732c68ebff46cbe68
Carlos Barra <carloseduardobc27@gmail.com>
2025-08-04 04:36:28 -0500

    Se corrigieron las funciones, falta implementar cambios en la funcion sortearAmigo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let amigoSecreto = '';
+
 let listaDeAmigos = [];
 
 
@@ -9,12 +9,10 @@ function asignarTextoElemento(Elemento, texto){
     titulo.innerHTML = texto;
 }
 
-function condicionesIniciales() {
+
     asignarTextoElemento('h1', 'Amigo Secreto');
     asignarTextoElemento('h2', 'Digite el nombre de sus Amigos');
     
-}
-
 //corregir codigo en proceso
 function agregarAmigo() {
     // Obtener el valor del campo de entrada y agregarlo a la lista
@@ -27,19 +25,38 @@ function agregarAmigo() {
         listaDeAmigos.push(amigoSecreto);
         actualizarLista();
         document.getElementById('amigo').value = ''; // Limpiar el campo de entrada
+        console.log(listaDeAmigos);
     }
 }
 
 function actualizarLista(){
-    let listaDeAmigos = document.getElementById('listaDeAmigos');
-    // Limpiar la lista antes de actualizarla
-    document.getElementById('listaDeAmigos') = '';
+    //obtener el elemento de la lista
+    let lista = document.getElementById('listaAmigos');
+    //limpiar la lista existente
+    lista.innerHTML = '';
     //iterar sobre el arreglo
-    for (let i = 0; i < listaDeAmigos.length; i++) {
-        let amigo = document.createElement('li');
-        amigo.textContent = listaDeAmigos[i];
-        listaDeAmigos.appendChild(amigo);
+    listaDeAmigos.forEach(function(amigo) {
+        //crear un nuevo elemento de lista
+        let li = document.createElement('li');
+        li.textContent = amigo; //asignar el nombre del amigo al elemento
+        lista.appendChild(li); //agregar el elemento a la lista
+    });
+    //agregar elementos a la lista, para cada amigo crear elemento de la lista
+    let actualizarLista = document.getElementById('listaAmigos');
+    console.log(actualizarLista);
+
+}
+
+function sortearAmigo() {
+        //validar que haya amigos disponibles
+    if (listaDeAmigos.length >= 2) {
+        //generar un indice aleatorio
+        let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+        //obtener el indice y mostrar el nombre del amigo
+        let amigoSeleccionado = listaDeAmigos[indiceAleatorio];
+        //mostrar el nombre del amigo secreto 
+        document.getElementById('resultado').textContent = `Tu amigo secreto es: ${amigoSeleccionado}`;
+    } else {
+        alert('Por favor, ingrese al menos dos amigos para realizar el sorteo.');
     }
-    //agregar elemento a la lista
-    document.getElementById('listaDeAmigos').appendChild(listaDeAmigos);
 }
